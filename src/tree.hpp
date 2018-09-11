970f7623605e75397bdfe1f98da6399b5846b8a0
ocornoc <ocornoc@protonmail.com>
2018-09-11 10:00:10 -0700

    Added basic data member declarations to finger_tree

--- src/tree.hpp
+++ src/tree.hpp
@@ -15,6 +15,10 @@ namespace fondue::finger_tree {
 template <class T, class Allocator> class fondue::finger_tree::tree {
 	typedef fondue::nonfunc::tree_2_3<T, Allocator> tree_2_3;
 	
-}
+	tree_2_3 prefix;
+	tree_2_3 suffix;
+	tree<T, Allocator> *child;
+};
+
 
 #endif
