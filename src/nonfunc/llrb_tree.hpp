125e124badc2a54a261397ac40c09f0d7bdf3836
ocornoc <ocornoc@protonmail.com>
2018-09-18 00:07:27 -0700

    Replaced namespace dec., new template

--- src/nonfunc/llrb_tree.hpp
+++ src/nonfunc/llrb_tree.hpp
@@ -5,9 +5,12 @@
 #include <new>
 #include <memory>
 
-namespace fondue::nonfunc {
-	template <typename T>
-	class llrb_tree;
+namespace fondue {
+	namespace nonfunc {
+		// keyT must have std::equal_to<keyT>, std::greater<keyT>, and std::less<keyT>
+		template <typename T, typename keyT = int>
+		class llrb_tree;
+	}
 }
 
 template <typename T>
