6d5275997720823ecda134403c0d4de7fc355928
ocornoc <ocornoc@protonmail.com>
2018-10-02 17:46:03 -0700

    Created tests folder, moved Makefile

---
+++ tests/composition/multi_type.cpp
@@ -0,0 +1,60 @@
+#include "../../src/composition.hpp"
+#include <iostream>
+#include <cstdint>
+#include <limits>
+using fondue::composition;
+
+typedef std::int64_t  firstf_arg_t;
+typedef std::uint64_t firstf_ret_t;
+
+typedef std::int64_t secondf_arg_t;
+typedef bool          secondf_ret_t;
+
+firstf_ret_t func1(firstf_arg_t n)
+{
+	if (n < 0) {
+		return -n;
+	} else {
+		return n;
+	}
+}
+
+secondf_ret_t func2(secondf_arg_t n)
+{
+	return n > std::numeric_limits<firstf_ret_t>::max();
+}
+
+static composition<firstf_ret_t(firstf_arg_t)> comp1(func1);
+static composition<secondf_ret_t(secondf_arg_t)> comp2(func2);
+static composition<secondf_ret_t(firstf_arg_t)> comp(comp2 * comp1);
+
+
+void test_composition(volatile firstf_arg_t n)
+{
+//	composition<firstf_ret_t(firstf_arg_t)> comp1(func1);
+//	composition<secondf_ret_t(secondf_arg_t)> comp2(func2);
+//	composition<secondf_ret_t(firstf_arg_t)> comp(comp2 * comp1);
+	
+	std::cout << "original: " << n << std::endl;
+	std::cout << "comp1(n): " << comp1(n).get() << std::endl;
+	std::cout << "comp(n):  " << comp(n).get() << std::endl;
+	
+	std::cout << std::endl;
+}
+
+int main()
+{
+	firstf_arg_t n = 0;
+	
+	std::cout << "Composition test" << std::endl \
+		  << "Simply input a non-integer to finish." << std::endl \
+		  << std::endl;
+	
+	while (true) {
+		std::cout << "Input an integer to test:\t";
+		std::cin >> n;
+		if (std::cin.fail()) break;
+		test_composition(n);
+		std::cout << std::endl;
+	}
+}
