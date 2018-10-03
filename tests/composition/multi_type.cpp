76d2838bc5c57dc15f4e39e6015187dfad0c346a
ocornoc <ocornoc@protonmail.com>
2018-10-03 08:56:01 -0700

    Removed interaction, fixed Makefile, rewrote tests

--- tests/composition/multi_type.cpp
+++ tests/composition/multi_type.cpp
@@ -1,7 +1,5 @@
 #include "../../src/composition.hpp"
-#include <iostream>
 #include <cstdint>
-#include <limits>
 using fondue::composition;
 
 typedef std::int64_t  firstf_arg_t;
@@ -24,33 +22,44 @@ secondf_ret_t func2(secondf_arg_t n)
 	return n > 10000;
 }
 
-static composition<firstf_ret_t(firstf_arg_t)> comp1(func1);
-static composition<secondf_ret_t(secondf_arg_t)> comp2(func2);
-static composition<secondf_ret_t(firstf_arg_t)> comp(comp2 * comp1);
-
-
-void test_composition(firstf_arg_t n)
+secondf_ret_t test_composition(firstf_arg_t n)
 {
-	std::cout << "original: " << n << std::endl;
-	std::cout << "comp1(n): " << comp1(n).get() << std::endl;
-	std::cout << "comp(n):  " << (comp(n).get() ? "true" : "false") << std::endl;
+	composition<firstf_ret_t(firstf_arg_t)> comp1(func1);
+	composition<secondf_ret_t(secondf_arg_t)> comp2(func2);
+	composition<secondf_ret_t(firstf_arg_t)> comp(comp2 * comp1);
 	
-	std::cout << std::endl;
+	volatile secondf_ret_t ret = comp(n).get();
+	
+	return ret;
 }
 
 int main()
 {
-	firstf_arg_t n = 0;
-	
-	std::cout << "Composition test" << std::endl \
-		  << "Simply input a non-integer to finish." << std::endl \
-		  << std::endl;
-	
-	while (true) {
-		std::cout << "Input an integer to test:\t";
-		std::cin >> n;
-		if (std::cin.fail()) break;
-		test_composition(n);
-		std::cout << std::endl;
+	for (int i = 9990; i <= 10000; i++) {
+		if (test_composition(i)) {
+			return 1;
+		}
 	}
+	
+	for (int i = 10010; i > 10000; i--) {
+		if (not test_composition(i)) {
+			return 1;
+		}
+	}
+	
+	for (int i = -9990; i >= -10000; i--) {
+		if (test_composition(i)) {
+			return 1;
+		}
+	}
+	
+	for (int i = -10010; i < -10000; i++) {
+		if (not test_composition(i)) {
+			return 1;
+		}
+	}
+	
+	return 0;
+	
+	test_composition(1);
 }
