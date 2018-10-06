fcec70dc730074cc13ec390dded575884ea0d9fa
ocornoc <ocornoc@protonmail.com>
2018-10-06 15:12:05 -0700

    Added some documentation to multi_type test

--- tests/composition/multi_type.cpp
+++ tests/composition/multi_type.cpp
@@ -26,9 +26,13 @@ secondf_ret_t func2(secondf_arg_t n)
 
 secondf_ret_t test_composition(firstf_arg_t n)
 {
+	// For this operation, we store it as a volatile because I'm not
+	// trying to test the compiler's ability to compute constexpr
+	// operations.
+	
 	composition<firstf_ret_t(firstf_arg_t)> comp1(func1);
 	composition<secondf_ret_t(secondf_arg_t)> comp2(func2);
-	composition<secondf_ret_t(firstf_arg_t)> comp(comp2 * comp1);
+	volatile composition<secondf_ret_t(firstf_arg_t)> comp(comp2 * comp1);
 	
 	volatile secondf_ret_t ret = comp(n).get();
 	
@@ -37,6 +41,10 @@ secondf_ret_t test_composition(firstf_arg_t n)
 
 int main()
 {
+	// These for loops are meant to test the behaviour of func2 * func1
+	// near its boundary point (defined as boundary_point). See func2's
+	// definition above for more details.
+	
 	for (int i = boundary_point - 10; i <= boundary_point; i++) {
 		if (test_composition(i)) {
 			return 1;
@@ -48,6 +56,8 @@ int main()
 			return 1;
 		}
 	}
+	
+	// These differ from the above for loops because
 	
 	for (int i = -boundary_point + 10; i >= -boundary_point; i--) {
 		if (test_composition(i)) {
