6229c58df22008979099ea90b47e5c5571b591a4
ocornoc <ocornoc@protonmail.com>
2018-10-06 17:35:28 -0700

    Added test for static composition object

---
+++ tests/composition/static_comp.cpp
@@ -0,0 +1,23 @@
+#include "../../src/composition.hpp"
+#include <functional>
+typedef int arg_t;
+typedef int ret_t;
+using fondue::composition;
+
+ret_t func(arg_t n)
+{
+	return n + 1;
+}
+
+static composition<ret_t(arg_t)> comp = std::function<ret_t(arg_t)>(func);
+
+int main()
+{
+	for (int i = 0; i < 100; i++) {
+		if (comp(i) != i + 1) {
+			return 1;
+		}
+	}
+	
+	return 0;
+}
